442c3a0cfbf7033297e065a8e722f5db9f68a821
Darryl Forbes <darrylforbes2022@u.northwestern.edu>
2021-04-27 23:00:36 -0500

    Add database fetching

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAafdw0RhUTXu_CLeerbD2H_EGeBuJ0mnI",
+  authDomain: "scheduler-cba67.firebaseapp.com",
+  databaseURL: "https://scheduler-cba67-default-rtdb.firebaseio.com",
+  projectId: "scheduler-cba67",
+  storageBucket: "scheduler-cba67.appspot.com",
+  messagingSenderId: "20674910557",
+  appId: "1:20674910557:web:f5e1a90d6e26d66c43b0b4"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export { firebase };
